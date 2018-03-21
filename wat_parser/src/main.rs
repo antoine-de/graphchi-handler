d300d745f1ef78598cbf8d26a78f423926580678
Antoine D <antoine.desbordes@gmail.com>
2018-03-21 16:46:07 +0100

    impossible touggly but working version

--- wat_parser/src/main.rs
+++ wat_parser/src/main.rs
@@ -13,6 +13,7 @@ extern crate structopt;
 #[macro_use(stmt)]
 extern crate cassandra_cpp;
 extern crate futures;
+extern crate itertools;
 
 use failure::Error;
 use std::path::PathBuf;
@@ -21,8 +22,9 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
 use std::env;
- use futures::future::Future;
+use futures::future::Future;
 use regex::Regex;
+use itertools::Itertools;
 
 #[derive(StructOpt, Debug)]
 struct Args {
@@ -31,19 +33,12 @@ struct Args {
     input: PathBuf,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Link {
-    url: String,
-    #[serde(default)]
-    text: String,
-    path: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Site {
-    url: String,
-    uuid: String,
-    links: Vec<Link>,
+fn format_link(json: &json::JsonValue) -> String {
+    format!("{{url:'{url}',text:'{text}',path:'{path}'}}", 
+        url=json["url"].as_str().unwrap_or(""),
+        text=json["text"].as_str().unwrap_or(""),
+        path=json["path"].as_str().unwrap_or(""),
+    )
 }
 
 fn parse_file(wat_file: PathBuf) -> Result<(), Error> {
@@ -55,7 +50,7 @@ fn parse_file(wat_file: PathBuf) -> Result<(), Error> {
     let match_record_id: Regex = Regex::new("\"WARC-Record-ID\":\"<urn:uuid:([^>]+)>\"").unwrap();
     let match_record_url: Regex = Regex::new("\"WARC-Target-URI\":\"([^\"]+)\"").unwrap();
     // to speed up a bit we don't parse the whole json at first, we parse only the links
-    let match_links: Regex = Regex::new("\"Links\":([^]]+])").unwrap();
+    let match_links: Regex = Regex::new("\"Links\":([^]]+}])").unwrap();
 
     let mut cluster = cassandra_cpp::Cluster::default();
     cluster.set_contact_points("127.0.0.1").unwrap();
@@ -71,35 +66,31 @@ fn parse_file(wat_file: PathBuf) -> Result<(), Error> {
             continue;
         }
 
-        info!("line: {}", l);
-
         let base_id = match_record_id.captures(&l).map(|c| c[1].to_string());
         let base_url = match_record_url.captures(&l).map(|c| c[1].to_string());
         let links = &match_links.captures(&l).map(|c| c[1].to_string());
-        info!("base id: {:?}", &base_id);
-        info!("base url: {:?}", &base_url);
-        info!("base links: {:?}", &links);
 
         if let &Some(ref l) = links {
-            let serde_json_links: Vec<Link> = serde_json::from_str(&l).unwrap_or(vec![]);
-            info!("serde_json_links parsed  links: {:?}", &serde_json_links);
-            let cassandra_link = Site {
-                url: base_url.unwrap_or("".into()),
-                uuid: base_id.unwrap_or("".into()),
-                links: serde_json_links,
-            };
-            info!("link to send: {:?}", &cassandra_link);
+            match json::parse(&l) {
+                Ok(parsed_json) => {
+                    let cassandra_links = format!("[{}]", parsed_json.members().into_iter().map(format_link).join(","));
 
-            let query = format!("INSERT INTO graph.sites (uuid, url, links) VALUES ({}, '{}', {})", 
-            base_url.unwrap_or("".into()), 
-            &cassandra_link.url, 
-            serde_json::to_string(&cassandra_link.links).unwrap());
+                    let query = format!("INSERT INTO graph.sites (uuid, url, links) VALUES ({}, '{}', {})", 
+                        base_id.unwrap_or("".into()), 
+                        base_url.unwrap_or("".into()), 
+                        cassandra_links
+                    );
 
-            info!("query {}", &query);
+                    info!("query {}", &query);
 
-            let statement = cassandra_cpp::Statement::new(&query, 0);
-            let mut future = session.execute(&statement);
-            future.wait().unwrap();
+                    let statement = cassandra_cpp::Statement::new(&query, 0);
+                    let mut future = session.execute(&statement);
+                    future.wait().unwrap();
+                },
+                Err(e) =>  {
+                    warn!("error while parsing the json, skipping line (error: {})", e);
+                }
+            }
         }
     }
 
